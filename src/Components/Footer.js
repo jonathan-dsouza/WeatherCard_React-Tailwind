3fe7e7e7e28c66d74f7e0d915f0b9934944334cf
Jonathan D'souza <jonathan07082001@gmail.com>
2023-08-30 13:01:53 +0530

    Fixed light mode footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <div className="fixed bottom-0 left-0 right-0 p-2 pb-4 text-center dark:bg-[#1a1a1b] shadow-xl dark:text-white">
+    <div className="fixed bottom-0 left-0 right-0 p-2 pb-4 text-center dark:bg-[#1a1a1b] shadow-xl dark:text-white bg-white">
       ⛅ Weather Card by Jonathan | Powered by OpenWeatherMap and Visual
       Crossing
     </div>
